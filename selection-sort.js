16b6742f26e157df8f6e17288156a83ada2ae906
Enrique Avalos <keex@Enriques-Air.lan>
2023-07-25 20:47:30 -0700

    added notes

--- selection-sort.js
+++ selection-sort.js
@@ -1,5 +1,8 @@
 // numbers [100,40,30,60,50,10] 
 // index   [  0, 1, 2, 3, 4, 5 ] 
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
+
 function selectionSort(nums){
     // outer loop
  for(let i = 0; i < nums.length-1; i++){
@@ -10,18 +13,14 @@ function selectionSort(nums){
         if(nums[j] < nums[minIndex]) {
             minIndex = j;
         }
-    //if the value at index 'j': (nums[j])
-    //is less than value at current: nums[minIndex],
-    //j becomes the minIndex: minIndex =j
+        // i and j are used as indices to navigate through the array, 
+        // while (nums[j] < nums[minIndex]) is an expression 
+        // that compares the values of elements at those indices.
     }
     [nums[i], nums[minIndex]] = [nums[minIndex], nums[i]];
-    //array destructuring (left)
-    //nums[i] element at current unsorted position (left)
-   //nums[minIndex] smallest element in the unsorted part (left)
-   //[nums[minIndex], nums[i]], value of the smallest element and (right)
-   //current unsorted element (right)
-   //(left becomes right)
+ 
 }
  return nums; 
 }
-console.log(selectionSort([100,40,30,60,50,10]))
+console.log(selectionSort([100,40,30,60,50,10]))  
+
